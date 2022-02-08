a492d734172705e5ff438822667653bb00d29d63
mukeshrawat02 <mukeshrawat02@gmail.com>
2022-02-08 12:23:27 +0000

    Added new password policy

--- src/passwordPolicy.js
+++ src/passwordPolicy.js
@@ -6,6 +6,23 @@ const doesPasswordMeetPolicy = (password) => {
   return true;
 };
 
+const doesPasswordMeetNewPolicy = (password) => {
+  /*
+   (?=(.*\d){2}) --> atleast 2 digits
+   (?=(.*[a-z]){2}) --> atleast 2 lower case chars
+   (?=(.*[A-Z]){2}) --> atleast 2 upper case chars
+  */
+
+  if (!password || !password.match(/^.{8,64}$/)) {
+    return false;
+  } else if(!password.match(/(?=(.*\d){2})(?=(.*[a-z]){2})(?=(.*[A-Z]){2})/)) {
+    return false;
+  }
+
+  return true;
+};
+
 module.exports = {
   doesPasswordMeetPolicy,
+  doesPasswordMeetNewPolicy
 };
