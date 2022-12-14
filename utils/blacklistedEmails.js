41fcf90037df9dd10a840b372741ad01a0b994a3
Ronald Repka <ronald.repka@gmail.com>
2022-12-14 15:01:30 +0000

    NSA-6146 add blacklisted email validation

--- utils/blacklistedEmails.js
+++ utils/blacklistedEmails.js
@@ -1,7 +1,7 @@
 // Use one of the following syntaxes local@domail.com or local@ or @domain.com
 const blacklistedEmails = [
   '@hotmail.com',
-  '@co.uk',
+  '@hotmail.co.uk',
   '@gmail.com',
   '@aol.com',
   '@yahoo.com',
