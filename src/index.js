176d910a2ef9ee5078889259ab1c8187ac01585a
stottidfe <peter.stott@education.gov.uk>
2023-11-21 17:01:00 +0000

    fix export

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const passwordPolicy = require('./passwordPolicy');
 const emailPolicy = require('./emailPolicy');
-const passwordHistory = require('./passwordHistory');
+const PasswordHistory = require('./passwordHistory');
 const {validate,testValidatorArray} = require('./validators');
 module.exports = {
   passwordPolicy,
   emailPolicy,
   validate,
   testValidatorArray,
-  passwordHistory
+  PasswordHistory
 };
