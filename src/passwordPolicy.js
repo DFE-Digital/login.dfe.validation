a610cfdb76969e18f65f29da483be34c4e895688
Ionut Predoi <ionut.predoi@education.gov.uk>
2024-04-12 09:32:28 +0100

    DSI-6850 - removed unused functionality

--- src/passwordPolicy.js
+++ src/passwordPolicy.js
@@ -1,26 +1,10 @@
 const dfeBlacklistedPasswords = require('../utils/blacklistedPasswords');
 const ncscBlacklistedPasswords = require('../utils/PwnedPasswordsTop100k.json');
 
-const doesPasswordMeetPolicy = (password) => {
+const doesPasswordMeetNewPolicy = (password) => {
   if (!password || !password.match(/^.{14,64}$/)) {
     return false;
   }
-  return true;
-};
-
-const doesPasswordMeetNewPolicy = (password) => {
-  /*
-   (?=(.*\d){2}) --> atleast 2 digits
-   (?=(.*[a-z]){2}) --> atleast 2 lower case chars
-   (?=(.*[A-Z]){2}) --> atleast 2 upper case chars
-  */
-
-  if (!password || !password.match(/^.{8,64}$/)) {
-    return false;
-  } if (!password.match(/(?=(.*\d){2})(?=(.*[a-z]){2})(?=(.*[A-Z]){2})/)) {
-    return false;
-  }
-
   return true;
 };
 
@@ -30,7 +14,6 @@ const isPasswordBlackListed = (password) => {
 };
 
 module.exports = {
-  doesPasswordMeetPolicy,
   doesPasswordMeetNewPolicy,
   isPasswordBlackListed,
 };
--- test/passwordPolicy/passwordPolicy.doesPasswordMeetPolicy.test.js
+++ test/passwordPolicy/passwordPolicy.doesPasswordMeetPolicy.test.js
@@ -3,22 +3,22 @@ const passwordPolicy = require('../../src/passwordPolicy');
 describe('When checking if a password meets the policy', () => {
   describe('and the password is a valid length of between 14 and 64 characters', () => {
     it('then it should return true if the password is 14 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('jumpSouthSpray');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('jumpSouthSpray');
 
       expect(actual).toBe(true);
     });
     it('then it should return true if the password is 14 characters long and includes numbers, uppercase letters, lowercase letters, symbols, and white spaces', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('A a12^&tes=()s');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('A a12^&tes=()s');
 
       expect(actual).toBe(true);
     });
     it('then it should return true if the password is 20 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('abcdefghijklm!*12365');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('abcdefghijklm!*12365');
 
       expect(actual).toBe(true);
     });
     it('then it should return true if the password is 64 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('abcdefghijklmnopqrstuvwxyz1234567890!@£$%^&*()012MskWas91K2!mao');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('abcdefghijklmnopqrstuvwxyz1234567890!@£$%^&*()012MskWas91K2!mao');
 
       expect(actual).toBe(true);
     });
@@ -26,81 +26,15 @@ describe('When checking if a password meets the policy', () => {
 
   describe('and the password is too short', () => {
     it('then is should return false if the password is 13 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('abc123!ef4623');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('abc123!ef4623');
 
       expect(actual).toBe(false);
     });
     it('then is should return false if the password is 6 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('abc123!ef46');
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('abc123!ef46');
 
       expect(actual).toBe(false);
     });
-    it('then is should return false if the password is blank', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('');
-
-      expect(actual).toBe(false);
-    });
-    it('then is should return false if the password is null', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy(null);
-
-      expect(actual).toBe(false);
-    });
-    it('then is should return false if the password is undefined', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy(undefined);
-
-      expect(actual).toBe(false);
-    });
-  });
-
-  describe('and the password is too long', () => {
-    it('then is should return false if the password is 65 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetPolicy('abcdefghijklmnopqrstuvwxyz1234567890!@£$%^&*()012MskWas91K2!maoef461');
-
-      expect(actual).toBe(false);
-    });
-  });
-});
-
-describe('When checking if a password meets the new policy', () => {
-  describe('and the password is a valid length of between 8 and 64 characters', () => {
-    it('then it should return true if the password is 8 characters and valid', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('12QWaszx');
-
-      expect(actual).toBe(true);
-    });
-
-    it('then it should return true if the password is 64 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('12QWaszxnopqrstuvwxyz1234567890!@£$%^&*()012MskWas91K2!mao');
-
-      expect(actual).toBe(true);
-    });
-  });
-
-  describe('and the password is too short', () => {
-    it('then is should return false if the password is 7 characters', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('12QWasz');
-
-      expect(actual).toBe(false);
-    });
-
-    it('then is should return false if the password does not have atleast 2 digits', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('QWaszxqw');
-
-      expect(actual).toBe(false);
-    });
-
-    it('then is should return false if the password does not have atleast 2 upper case chars', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('12aszxqw');
-
-      expect(actual).toBe(false);
-    });
-
-    it('then is should return false if the password does not have atleast 2 lower case chars', () => {
-      const actual = passwordPolicy.doesPasswordMeetNewPolicy('12QWASZX');
-
-      expect(actual).toBe(false);
-    });
-
     it('then is should return false if the password is blank', () => {
       const actual = passwordPolicy.doesPasswordMeetNewPolicy('');
 
@@ -113,6 +47,14 @@ describe('When checking if a password meets the new policy', () => {
     });
     it('then is should return false if the password is undefined', () => {
       const actual = passwordPolicy.doesPasswordMeetNewPolicy(undefined);
+
+      expect(actual).toBe(false);
+    });
+  });
+
+  describe('and the password is too long', () => {
+    it('then is should return false if the password is 65 characters', () => {
+      const actual = passwordPolicy.doesPasswordMeetNewPolicy('abcdefghijklmnopqrstuvwxyz1234567890!@£$%^&*()012MskWas91K2!maoef461');
 
       expect(actual).toBe(false);
     });
