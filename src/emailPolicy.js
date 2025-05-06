cf8063e9fe862e404cbaff2193ed5b2c746e4584
Julio Velezmoro <julio28ve@gmail.com>
2025-05-06 18:48:31 +0100

    Format with `format` command.

--- src/emailPolicy.js
+++ src/emailPolicy.js
@@ -1,21 +1,21 @@
-const emailValidator = require('email-validator');
-const blacklistedEmails = require('../utils/blacklistedEmails');
+const emailValidator = require("email-validator");
+const blacklistedEmails = require("../utils/blacklistedEmails");
 
-const blacklistedEmailPatterns = blacklistedEmails
-  .map((blackListedEmail) => {
-    const fragments = blackListedEmail.split('@');
-    if (fragments[0] === '') {
-      return RegExp(`@${fragments[1]}$`, 'i');
-    } else if (fragments[1] === '') {
-      return RegExp(`^${fragments[0]}[.\\-_]?\\d*@`, 'i');
-    } else {
-      return RegExp(`^${fragments[0]}[.\\-_]?\\d*@${fragments[1]}$`, 'i');
-    }
-  });
+const blacklistedEmailPatterns = blacklistedEmails.map((blackListedEmail) => {
+  const fragments = blackListedEmail.split("@");
+  if (fragments[0] === "") {
+    return RegExp(`@${fragments[1]}$`, "i");
+  } else if (fragments[1] === "") {
+    return RegExp(`^${fragments[0]}[.\\-_]?\\d*@`, "i");
+  } else {
+    return RegExp(`^${fragments[0]}[.\\-_]?\\d*@${fragments[1]}$`, "i");
+  }
+});
 
 const doesEmailMeetPolicy = (email) => emailValidator.validate(email);
 
-const isBlacklistedEmail = (email) => blacklistedEmailPatterns.some(pattern => pattern.test(email));
+const isBlacklistedEmail = (email) =>
+  blacklistedEmailPatterns.some((pattern) => pattern.test(email));
 
 module.exports = {
   doesEmailMeetPolicy,
